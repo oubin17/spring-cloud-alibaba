d3ccd10f5144e3e331fe547e8e0fb58d7e54f2df
oubin <oubin.ob@antgroup.com>
2021-09-05 18:11:38 +0800

    Init

---
+++ src/main/java/microservice/springcloudalibaba/SpringCloudAlibabaApplication.java
@@ -0,0 +1,13 @@
+package microservice.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaApplication.class, args);
+    }
+
+}
